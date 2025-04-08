1dd8c9a265634fa31937584c4854026c1e886351
Maurice Ford <mauriceford@gmail.com>
2025-04-08 20:14:52 +0000

    testing api

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
-require "sinatra"
-require "sinatra/reloader"
+# require "sinatra"
+# require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+# get("/") do
+#   "
+#   <h1>Welcome to your Sinatra App!</h1>
+#   <p>Define some routes in app.rb</p>
+#   "
+# end
+require 'uri'
+require 'net/http'
+require 'http'
+require 'json'
+require 'dotenv/load'
+
+fkey = ENV.fetch("FORTUNE_KEY")
+
+url = URI("https://fortune-cookie4.p.rapidapi.com/")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["x-rapidapi-key"] = fkey
+request["x-rapidapi-host"] = 'fortune-cookie4.p.rapidapi.com'
+
+response = http.request(request)
+reader =  response.read_body
+# jsontest = JSON.parse(reader)
+hashtest = {"welcome" => :tester}
+puts reader.class
+puts hashtest.class
+puts hashtest["welcome"]
+puts reader
